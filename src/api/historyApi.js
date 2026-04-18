ec82681fb7ba4efa6219e4358a70d6696a6a0113
Nitish Kumar <ns8231001@gmail.com>
2026-04-18 11:27:02 +0530

    Update historyApi.js

--- src/api/historyApi.js
+++ src/api/historyApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { getToken } from "../utils/auth";
 
-const BASE_URL = "http://localhost:8080"; // gateway
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8080"; // gateway
 
 export const getUserHistory = async () => {
   const token = getToken();
